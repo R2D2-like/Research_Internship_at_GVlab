f16a0577fa9bb4d533d52506dc0805fa0331487e
R2D2-like <tsukurobokun@gmail.com>
2024-03-31 04:31:20 +0900

    data0313 trial 4 and fix bug

--- scripts/pre-process_demo_data_proposed.py
+++ scripts/pre-process_demo_data_proposed.py
@@ -3,12 +3,15 @@ from config.values import *
 import os
 
 dir = '/root/Research_Internship_at_GVlab/data0313/real/step2/data/'
+save_dir = '/root/Research_Internship_at_GVlab/data0402/real/step2/data/'
 
 dataset = {}
-ft_data, z_diff_data = None, None
+ft_data_log, z_diff_data_log = None, None
+
 for sponge in TRAIN_SPONGES_LIST:
-    # ft_data, z_diff_data = None, None
+    ft_data, z_diff_data = None, None
     for trial in range(1, DATA_PER_SPONGE+1):
+        trial = 4
         data_path = dir + sponge + '_' + str(trial) + '.npz'
         if not os.path.exists(data_path):
             print('The data for', sponge, 'does not exist.')
@@ -29,14 +32,24 @@ for sponge in TRAIN_SPONGES_LIST:
         else:
             z_diff_data = np.concatenate([z_diff_data, np.expand_dims(z_diff, axis=0)], axis=0) #(trial, 99)
             ft_data = np.concatenate([ft_data, np.expand_dims(ft, axis=0)], axis=0) #(trial, 100, 6)
+    print('ft_data', ft_data.shape)
+    if ft_data_log is None:
+        ft_data_log = ft_data 
+        z_diff_data_log = z_diff_data
+    else:
+        ft_data_log = np.concatenate([ft_data_log, ft_data], axis=0)
+        z_diff_data_log = np.concatenate([z_diff_data_log, z_diff_data], axis=0)
+    print('ft_data_log', ft_data_log.shape)
+
+
     dataset[sponge] = {'z_diff': z_diff_data, 'ft': ft_data}
 
 # calculate the max and min values
-min_z_diff, max_z_diff = np.min(z_diff_data), np.max(z_diff_data)
+min_z_diff, max_z_diff = np.min(z_diff_data_log), np.max(z_diff_data_log)
 min_ft, max_ft = [], []
-for i in range(ft_data.shape[2]):
-    max_ft.append(np.max(ft_data[:, :, i]))
-    min_ft.append(np.min(ft_data[:, :, i]))
+for i in range(ft_data_log.shape[2]):
+    max_ft.append(np.max(ft_data_log[:, :, i]))
+    min_ft.append(np.min(ft_data_log[:, :, i]))
 
 # normalize the data
 z_diff_dataset = {}
@@ -53,8 +66,8 @@ for sponge in TRAIN_SPONGES_LIST:
     ft_dataset[sponge] = normalized_ft
 
 # save as npz
-np.savez(dir + 'demo_preprocessed_z_diff.npz', **z_diff_dataset)
-np.savez(dir + 'demo_preprocessed_ft.npz', **ft_dataset)
+np.savez(save_dir + 'demo_preprocessed_z_diff.npz', **z_diff_dataset)
+np.savez(save_dir + 'demo_preprocessed_ft.npz', **ft_dataset)
 print('Copy the value below and paste it to config/values.py')
 print('DEMO_Z_DIFF_MIN =', min_z_diff)
 print('DEMO_Z_DIFF_MAX =', max_z_diff)
